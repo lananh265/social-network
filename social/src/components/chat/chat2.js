07a62de506b1a7d39285fc5d9b999f9ce5e15ed2
lananh265 <htlanhd20105@cusc.ctu.edu.vn>
2021-09-12 22:57:21 +0700

    chat real time

--- social/src/components/chat/chat2.js
+++ social/src/components/chat/chat2.js
@@ -1,32 +1,118 @@
-import { SettingsSystemDaydream } from "@material-ui/icons";
-import { useState, useRef, useEffect } from "react";
-import socketIOClient from "socket.io-client"
+
+// import { useState, useRef, useEffect } from "react";
+// import socketIOClient, { Socket } from "socket.io-client"
+// const host  = "http://localhost:4000"
+// export default function Chat(){
+// const [id, setId] = useState("")//id bam duoc cap boi  Server Socket
+// const [tn, setTN] = useState("")//tn la noi dung tin nhan nguoi dung nhap
+// const [noiDung, setNoiDung] = useState([])//noiDung la list tin nhan cu va moi
+
+// const xuLyTN = (e)=>{
+//   setTN(e.target.value)
+// }//luu gia tri nguoi dung nhap vao bien tn
+
+// const guiTN = ()=>{
+//   const ob= {
+//     id: id,
+//     message: tn
+//   }
+//   socketRef.current.emit("chatAnh",ob)
+//   setTN("")
+// }//phat tin hieu emit cho Server xu li
+
+// const listTN = noiDung.map ( (e, index)=>
+//   <div key = {index}>
+//     {e.id} {e.message}
+//   </div>
+// )
+
+// let socketRef= useRef()
+
+// useEffect( ()=>{
+// const url= "http://localhost:4000/get_messages"
+// fetch(url,{
+//   method: "GET"
+// })
+// .then((data)=>data.json())
+// .then( (dataJson)=>{
+//   setNoiDung(dataJson)
+//   // console.log(dataJson)
+// })
+// .catch( (e)=>{console.log(e)})
+
+//   socketRef.current = socketIOClient.connect(host)
+//   socketRef.current.on("nhanID", (data)=>{
+//     setId(data)
+//   })
+
+//   socketRef.current.on("chatTy", (data)=>{
+//     setNoiDung(noiDung =>[...noiDung, data])
+//     //them ob moi vao noi dung tin nhan
+
+//   })
+
+// },[])
+//   return(
+//     <div>
+//       <h1>Day la Chat cho id</h1>
+//       {id}
+//       {listTN}
+//       <div>
+//         <form>
+//           <label>
+//             <input type="text" value={tn} onChange={(e)=>{xuLyTN(e)}}/>
+//           </label>
+//         </form>
+//         <button onClick={()=>{guiTN()}}> Send</button>
+//       </div>
+     
+//     </div>
+//   )
+
+// }
+
+
+import {useState, useRef, useEffect} from 'react'
+import socketIOClient, { Socket } from "socket.io-client"
+const host = "http://171.235.73.143:4000/"
 
 export default function Chat(){
-  const [tinNhan, settinNhan] = useState("");
-  let dem=useRef(0);
-  const xuLy = ()=>{
-    // settinNhan(tinNhan + "a-")
-    dem.current++;
-    console.log(dem.current);
-    
+  const [id, setID] = useState("")
+  const [tn, setTN] = useState ("")
+  const luuTN = (e)=>{
+    setTN(e.target.value)
   }
+const guiTN = ()=>{
+  const ob={
+    id_gui:3,
+    id_nhan:2,
+    message: tn
+  }
+  socketRef.current.emit("guiTN", ob)
   
+}
 
 
- let socketRef = useRef();
+  let socketRef = useRef()
+  useEffect( ()=>{
+    socketRef.current = socketIOClient.connect(host)
+    socketRef.current.on("nhanID", (data)=>{
+      setID(data)
+    }) 
 
-useEffect( ()=>{
-  console.log ("render Chat")
-
-
-},[])
+  },[])
   return(
     <div>
-      <h1>Day la Chat</h1>
-      {tinNhan}
-       <button onClick = {()=>{xuLy()}}>Thay doi noi dung State</button>
+      <h1>Day La Chat </h1>
+      {id}
+      <div>
+        <form>
+          <label>
+            <input type="text" value={tn} onChange={(e)=>{luuTN(e)}} />
+          </label>
+        </form>
+        <button onClick={()=>{guiTN()}}>Gui</button>
+      </div>
     </div>
   )
-
 }
