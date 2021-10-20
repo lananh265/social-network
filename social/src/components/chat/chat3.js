4a7e82b25ac2b4d79c05f7453aced64f335d468a
lananh265 <htlanhd20105@cusc.ctu.edu.vn>
2021-10-21 01:09:48 +0700

    message

--- social/src/components/chat/chat3.js
+++ social/src/components/chat/chat3.js
@@ -5,10 +5,17 @@ import GetAvatar from "../../API/GetAvatar";
 import GetInfor from "../../API/GetInfor"
 const src="http://localhost:1337/server-node/v0.1/server/images/avatars/"
 const host ="http://localhost:4000/"
+
+const obTarget = {
+  target_id: "",
+  name: "",
+  id: ""
+}
 export default function Chat(){
+  const [target, setTarget] = useState(obTarget)
   const [avatar, setAvatar] = useState({})
-  const tokenString = localStorage.getItem('token');
-  const token = JSON.parse(tokenString);
+  // const tokenString = localStorage.getItem('token');
+  // const token = JSON.parse(tokenString);
 
     const [id, setId] = useState()
     const [listTN, setlistTN] = useState([])
@@ -16,30 +23,29 @@ export default function Chat(){
     const [target_id, setTarget_ID] = useState("")
     const messagesEnd = useRef();
 
+    const inputTarget = (e)=>{
+      const inputName = e.currentTarget.name;
+      const value = e.currentTarget.value;
+      setTarget(prev => ({ ...prev, [inputName]: value }));  
+  }
+    const {token} = GetInfor()
     let socketRef = useRef()
     const [connecter_id, setConnecter_ID] = useState("")//Lan Anhnh
     // const [target_id, setTarget_ID] = useState("2")//ty
 
     const Send = ()=>{
-        if(tn.length>0){
-        // let ob = {
-        //     id:id,
-        //     name: target_id ==="1" ? "Lan Anh" : "Ty",
-        //     connecter_id: target_id==="1" ? "2" : "1" ,
-        //     target_id: target_id,
-        //     text_me: tn
-        // }
-        let ob = {
-          id:id,
-          name: target_id ==="1" ? "Lan Anh" : "Ty",
-          connecter_id: "1",
-          target_id: "2",
-          text_me: tn
+      if(tn.length>0){
+          let ob = {
+              id: id,
+              name: token.name,
+              connecter_id: token.id,
+              target_id: target.target_id,
+              text_me: tn
+          }
+          socketRef.current.emit("mess-out",ob)
+          setTN("")
       }
-        socketRef.current.emit("mess-out",ob) //kenh chat: messout
-        setTN("")
-    }
-}
+  }
 
 const scrollToBottom = () => {
     messagesEnd.current.scrollIntoView({ behavior: "smooth" });
@@ -51,18 +57,47 @@ const renderMess =  listTN.map((m, index) =>
 </div>
 )
 
+    // useEffect( ()=>{
+    //     socketRef.current = socketIOClient.connect(host)
+    //     socketRef.current.on("getId",(data)=>{
+    //         setId(data)
+    //     })
+    //     socketRef.current.on("mess-in", (data)=>{
+    //         setlistTN((listTN)=> [...listTN,data])
+    //         console.log(data)
+    //         console.log(listTN)
+    //         scrollToBottom()
+    //     })
+    // }, [])
+
     useEffect( ()=>{
-        socketRef.current = socketIOClient.connect(host)
-        socketRef.current.on("getId",(data)=>{
-            setId(data)
-        })
-        socketRef.current.on("mess-in", (data)=>{
-            setlistTN((listTN)=> [...listTN,data])
-            console.log(data)
-            console.log(listTN)
-            scrollToBottom()
-        })
-    }, [])
+      socketRef.current = socketIOClient.connect(host)
+      socketRef.current.on("getId", (data)=>{
+          if(!data){alert("Cảnh báo: ai đó đã đăng nhập vào tài khoản của bạn !")}
+          else{
+              const obSocket = {
+                  username: token.username,
+                  connecter_id: token.id,
+                  socket_id: data
+              }
+              socketRef.current.emit("client-send-obSocket",obSocket)
+          }
+          setId(data)
+      })
+      
+      setTimeout(function() {
+          console.log(id)
+        }, 500); //time out
+      
+      
+      socketRef.current.on("mess-in", (data)=>{
+          setlistTN( (listTN)=>[...listTN,data])
+          scrollToBottom()
+      })
+  },[])  
+
+
+    
     useEffect( ()=>{
       let mounted = true;
       let ob = {
@@ -113,6 +148,11 @@ const renderMess =  listTN.map((m, index) =>
       <img className={s.shareProfileImge} src={`${avatar.src}?${avatar.imageHash}`} alt="" />
       </div>
       </div>
+      <div>
+                {/* {id} */}
+                <br/>
+                <input type="number" name="target_id" value={target.target_id} onChange={(e)=>{inputTarget(e)}} />
+            </div>
         <div className={`${s.boxchat}`}>
         
         <div className={`${s.boxchatmessage}`}>
